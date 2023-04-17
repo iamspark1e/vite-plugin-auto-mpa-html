6e55e25a81ee2e1b51c360ef89ed3e81d46e7a4b
spark1e <wenbo@sparkle.im>
2023-04-17 11:18:35 +0000

    fix: simplify github action & fix test fixtures config

--- tests/fixtures/vite.config.js
+++ tests/fixtures/vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import autoMpaHTMLPlugin from '../../dist/index'
+import autoMpaHTMLPlugin from '../../dist/index.js'
 
 // https://vitejs.dev/config/
 export default defineConfig({
